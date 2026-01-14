bf8e21a49cdb6fcc62c425dd587ebd09705f88b0
AlexSinX <alexsin1712@yandex.ru>
2026-01-14 22:32:33 +0300

    Add JaCoCo

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -6,12 +6,49 @@ import org.junit.jupiter.api.Assertions;
 
 public class StatisticsServiceTest {
 
+
     @Test
     void findMax() {
         StatisticsService service = new StatisticsService();
 
-        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12}; // максимальное значение находится вначале цикла.
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_LastElementIsMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 15}; // максимальное значение находится в конце цикла.
+        long expected = 15;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_MiddleElementIsMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {5, 8, 4, 5, 3, 20, 8, 6, 11, 11, 12}; // максимальное значение находится в середине цикла.
+        long expected = 20;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_AllElementsAreEqual() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {7, 7, 7, 7}; // все элементы равны.
+        long expected = 7;
 
         long actual = service.findMax(incomesInBillions);
 
